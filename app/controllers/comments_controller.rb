2f2ea5b6bde73ba54a5a01d67eea953d80cf1132
Arnas Fomenko <arnas@coingate.com>
2017-09-01 14:31:13 +0300

    Render fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,20 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
 
   def index
-    @article = Article.find(params[:article_id])
-    @comments = @article.comments.all
-    render json: serialized_comments
+    render json: (
+      Comment.where(article_id: params[:article_id]).map do |comment|
+        {
+          id: comment.id,
+          body: comment.body,
+          article_id:comment.article_id,
+          date: comment.created_at.strftime('%-H:%-M:%-S %-b %-d, %Y'),
+          commenter: {
+            user_id: comment.user.id,
+            user_email: comment.user.email
+          }
+        }
+      end
+    )
   end
 
 
@@ -30,21 +41,6 @@ class CommentsController < ApplicationController
   private
     def comment_params
       params.require(:comment).permit(:body)
-    end
-
-    def serialized_comments
-      @comments.map do |item|
-        {
-          id: item.id,
-          body: item.body,
-          article_id:item.article_id,
-          date: item.created_at.strftime('%-H:%-M:%-S %-b %-d, %Y'),
-          commenter: {
-            user_id: item.user.id,
-            user_email: item.user.email
-          }
-        }
-      end
     end
 
 end
